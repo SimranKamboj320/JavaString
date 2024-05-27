aa0a49d24d1dc466635d4cce8598d0c5f1bb86fa
SimranKamboj320 <simrankamboj320@gmail.com>
2024-05-28 00:25:24 +0530

    String Length

---
+++ StringLength.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class JavaStrings{
+
+    public static void main(String args[]) {
+        String str = "abcd";
+        System.out.println(str.length());
+    }
+}    
