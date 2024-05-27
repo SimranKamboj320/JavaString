021ae126f0a08be1c8f2a3ceea614cb2575c81de
SimranKamboj320 <simrankamboj320@gmail.com>
2024-05-28 00:25:44 +0530

    String Concatenation

---
+++ Concatenate.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class JavaStrings{
+
+    public static void main(String args[]) {
+        String firstName = "Simran";
+        String LastName = "Kamboj";
+        String Name = firstName + " " + LastName;
+        System.out.println(Name);
+    }
+}    
