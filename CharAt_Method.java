ec8258f4d5d98f5826857a597421be61ae136605
SimranKamboj320 <simrankamboj320@gmail.com>
2024-05-28 00:26:10 +0530

    CharAt Method
    CharAt Method in Java

---
+++ CharAt_Method.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class JavaStrings{
+
+    public static void printLetters(String str) {
+        for(int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i)+ " ");
+        }
+        System.out.println();
+    }
+    
+    public static void main(String args[]){
+        String firstName = "Simran";
+        String LastName = "Kamboj";
+        String Name = firstName + " " + LastName;
+        printLetters(Name);
+    }
+}   
